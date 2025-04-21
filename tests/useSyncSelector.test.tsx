ba529594826033b770bd7dff95ae00419b6feca8
GitHub <rakshitbharat@gmail.com>
2025-04-21 23:06:04 +0530

    -format

--- tests/useSyncSelector.test.tsx
+++ tests/useSyncSelector.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, act } from '@testing-library/react';
 import { createSyncStore } from '../src/createSyncStore';
 import { Reducer } from '../src/types';
